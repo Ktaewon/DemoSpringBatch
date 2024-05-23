5f84f66f9f8d3916e6e9c865684b583ce108d28b
Ktaewon <dydalehdxo@naver.com>
2024-05-23 17:14:24 +0900

    next 알아보기

--- src/main/kotlin/com/kakao/demobatch/job/StepNextJobConfiguration.kt
+++ src/main/kotlin/com/kakao/demobatch/job/StepNextJobConfiguration.kt
@@ -3,56 +3,59 @@ package com.kakao.demobatch.job
 import com.kakao.demobatch.util.logger
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
-import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.repeat.RepeatStatus
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
 
 @Configuration
-class SimpleJobConfiguration {
+class StepNextJobConfiguration {
     private val logger = logger()
 
     @Bean
-    fun simpleJob(
+    fun stepNextJob(
         jobRepository: JobRepository,
         transactionManager: PlatformTransactionManager,
     ): Job {
-        return JobBuilder("simpleJob", jobRepository)
-            .start(simpleStep1(jobRepository, transactionManager, null))
-            .next(simpleStep2(jobRepository, transactionManager, null))
+        return JobBuilder("stepNextJob", jobRepository)
+            .start(step1(jobRepository, transactionManager))
+            .next(step2(jobRepository, transactionManager))
+            .next(step3(jobRepository, transactionManager))
             .build()
     }
 
     @Bean
-    @JobScope
-    fun simpleStep1(
+    fun step1(
         jobRepository: JobRepository,
         transactionManager: PlatformTransactionManager,
-        @Value("#{jobParameters[requestDate]}") requestDate: String?,
-    ): Step = StepBuilder("simpleStep1", jobRepository)
+    ): Step = StepBuilder("step1", jobRepository)
         .tasklet ({ _, _ ->
             logger.info(">>>>> This is Step1")
-            logger.info(">>>>> requestDate = {}", requestDate)
             return@tasklet RepeatStatus.FINISHED
-//            throw IllegalArgumentException("step1에서 실패합니다.")
         }, transactionManager)
         .build()
 
     @Bean
-    @JobScope
-    fun simpleStep2(
+    fun step2(
         jobRepository: JobRepository,
         transactionManager: PlatformTransactionManager,
-        @Value("#{jobParameters[requestDate]}") requestDate: String?,
-    ): Step = StepBuilder("simpleStep2", jobRepository)
+    ): Step = StepBuilder("step2", jobRepository)
         .tasklet({ _, _ ->
             logger.info(">>>>> This is Step2")
-            logger.info(">>>>> requestDate = {}", requestDate)
+            return@tasklet RepeatStatus.FINISHED
+        }, transactionManager)
+        .build()
+
+    @Bean
+    fun step3(
+        jobRepository: JobRepository,
+        transactionManager: PlatformTransactionManager,
+    ): Step = StepBuilder("step3", jobRepository)
+        .tasklet({ _, _ ->
+            logger.info(">>>>> This is Step3")
             return@tasklet RepeatStatus.FINISHED
         }, transactionManager)
         .build()
